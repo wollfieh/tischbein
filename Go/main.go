463250802edbc532ee18506480f1bf8ae5181750
Wolfgang Braun <wolfgang.braun@gmx.de>
2023-04-30 00:10:26 +0200

    parameter

--- Go/main.go
+++ Go/main.go
@@ -6,16 +6,25 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
 	hostname, _ := os.Hostname()
 	fmt.Printf("got / request\n")
-	io.WriteString(w, "This is my website! on "+hostname+"\n")
+	io.WriteString(w, "This is my website! on "+hostname+" with tag "+strings.ToUpper(getTag())+"\n")
 }
 func getHello(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got /hello request\n")
 	io.WriteString(w, "Hello, HTTP!\n")
+}
+
+func getTag() string {
+	r := os.Getenv("FOO")
+	if r == "" {
+		return "default"
+	}
+	return r
 }
 
 func main() {
